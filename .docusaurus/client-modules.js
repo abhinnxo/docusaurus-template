9ac6cacd287151b59f9228bfc6482c82ac0ee0df
Abhinn Krishn <abhinnk21@gmail.com>
2023-12-07 21:02:44 +0530

    UPDATE: docs dir

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("D:\\Coding\\Docsaurus\\docusaurus-template\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("D:\\Coding\\Docsaurus\\docusaurus-template\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("D:\\Coding\\Docsaurus\\docusaurus-template\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("D:\\Coding\\Docsaurus\\docusaurus-template\\src\\css\\custom.css"),
+];
